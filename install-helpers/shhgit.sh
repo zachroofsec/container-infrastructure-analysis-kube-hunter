05d580dd9d8293a9b1a85437bbaa8d41b603955d
zachroofsec <zroof85@gmail.com>
2021-03-19 11:20:52 -0400

    Updates

--- install-helpers/shhgit.sh
+++ install-helpers/shhgit.sh
@@ -39,15 +39,15 @@ SSHGIT_CONFIG_PATH=/tmp/config.yaml
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin
 
-#apt-get update && apt-get install -y git &&
-#    wget https://storage.googleapis.com/golang/go1.11.1.linux-amd64.tar.gz -O "${GO_DOWNLOAD_PATH}" &&
-#    tar -C /usr/local -xzf "${GO_DOWNLOAD_PATH}" &&
-#    rm -rf "$GO_DOWNLOAD_PATH" &&
-#    mkdir -p "${GOPATH}"
+apt-get update && apt-get install -y git &&
+    wget https://storage.googleapis.com/golang/go1.11.1.linux-amd64.tar.gz -O "${GO_DOWNLOAD_PATH}" &&
+    tar -C /usr/local -xzf "${GO_DOWNLOAD_PATH}" &&
+    rm -rf "$GO_DOWNLOAD_PATH" &&
+    mkdir -p "${GOPATH}"
 #
 ### Install shhgit binary and configuration file
-#go get -v github.com/eth0izzle/shhgit
-#wget https://raw.githubusercontent.com/eth0izzle/shhgit/3ce441853d999dacf6e20e59b116c135dcdd0c68/config.yaml -O "${SSHGIT_CONFIG_PATH}"
+go get -v github.com/eth0izzle/shhgit
+wget https://raw.githubusercontent.com/eth0izzle/shhgit/3ce441853d999dacf6e20e59b116c135dcdd0c68/config.yaml -O "${SSHGIT_CONFIG_PATH}"
 
 # +--------------------+
 # RUN
